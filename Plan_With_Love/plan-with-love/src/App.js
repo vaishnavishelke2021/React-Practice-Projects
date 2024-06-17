76dcabe90c9524cb20a64761f34b75429aa775bf
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-17 22:20:59 +0530

    refresh added

--- Plan_With_Love/plan-with-love/src/App.js
+++ Plan_With_Love/plan-with-love/src/App.js
@@ -5,9 +5,11 @@ function App() {
   return (
     <div>
       <div className="">
-        <h1 className="text-slate-300 mx-auto w-fit  text-center px-8 py-3 mt-14 tracking-normal rounded-md bg-zinc-800/60 text-[1.9rem]  font-poetsen [word-spacing:5px] shadow-lg">
+        <h1 className="text-slate-300 mx-auto w-fit  text-center px-8 py-4 mt-14 tracking-normal rounded-md bg-zinc-800/60 text-[1.9rem]  font-poetsen [word-spacing:5px] shadow-lg">
           Plan With Love{" "}
         </h1>
+
+        
         <CardContainer />
       </div>
     </div>
--- Plan_With_Love/plan-with-love/src/Components/CardContainer.js
+++ Plan_With_Love/plan-with-love/src/Components/CardContainer.js
@@ -7,17 +7,27 @@ function CardContainer() {
 
   function removeCardHandler(id) {
     const filteredCard = filterCard.filter((card) => card.id !== id);
-    setFilterCard([...filteredCard]); 
+    setFilterCard([...filteredCard]);
   }
 
-  return (
+  return filterCard.length === 0 ? (
+    <div className="mt-6 w-[90%] min-h-[70vh] py-4 bg-zinc-800 rounded-sm flex flex-col flex-wrap justify-center items-center mx-auto">
+      <h1 className="text-lg font-semibold text-white/80">No Tours Left</h1>
+      <button
+        onClick={() => setFilterCard(data)}
+        className="mt-4 font-medium hover:bg-zinc-950 transition-all  px-4 py-1 bg-zinc-700 text-slate-300 text-sm rounded-sm"
+      >
+        Refresh
+      </button>
+    </div>
+  ) : (
     <div>
-      <div className="mt-4 w-[90%] py-12 flex flex-wrap gap-10 justify-center mx-auto">
+      <div className="mt-6 w-[90%] py-12 flex flex-wrap gap-10 justify-center mx-auto">
         {filterCard.map((card) => (
           <Card
             key={card.id}
             data={card}
-            removeCardHandler={removeCardHandler} 
+            removeCardHandler={removeCardHandler}
           />
         ))}
       </div>
