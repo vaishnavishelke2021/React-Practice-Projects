53cdc1ff62266515268dfbd8586120868f16e790
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-17 17:16:10 +0530

    json and heading added

--- Plan_With_Love/plan-with-love/src/App.js
+++ Plan_With_Love/plan-with-love/src/App.js
@@ -3,7 +3,9 @@ import React from 'react'
 function App() {
   return (
     <div>
-      dd
+      <div className=''>
+        <h1 className='text-rose-400 mx-auto w-fit  text-center px-8 py-3 mt-10 tracking-normal rounded-md bg-zinc-800/60 text-[1.9rem]  font-poetsen [word-spacing:5px] shadow-lg'>Plan With Love </h1>
+      </div>
     </div>
   )
 }
--- Plan_With_Love/plan-with-love/src/Components/Card.js
+++ Plan_With_Love/plan-with-love/src/Components/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Card() {
   return (
     <div>
-      ss
+      Lorem 
     </div>
   )
 }
