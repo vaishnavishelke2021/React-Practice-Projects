c3a9bd6488312097e4dac8a50d584ef5b0789340
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-17 19:40:02 +0530

    card added

--- Plan_With_Love/plan-with-love/src/App.js
+++ Plan_With_Love/plan-with-love/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Card from './Components/Card'
+import data from './Utils/mockdata.json'
 
 function App() {
   return (
     <div>
       <div className=''>
-        <h1 className='text-rose-400 mx-auto w-fit  text-center px-8 py-3 mt-10 tracking-normal rounded-md bg-zinc-800/60 text-[1.9rem]  font-poetsen [word-spacing:5px] shadow-lg'>Plan With Love </h1>
+        <h1 className='text-slate-100 mx-auto w-fit  text-center px-8 py-3 mt-16 tracking-normal rounded-md bg-zinc-800/60 text-[1.9rem]  font-poetsen [word-spacing:5px] shadow-lg'>Plan With Love </h1>
+        <div className='mt-3 w-[90%] py-12 flex flex-wrap gap-10 justify-center mx-auto'>
+          
+          {
+            data.map((card)=>(
+              <Card key={card.id} data={card} />
+            ))
+          }
+        </div>
       </div>
     </div>
   )
--- Plan_With_Love/plan-with-love/src/Components/Card.js
+++ Plan_With_Love/plan-with-love/src/Components/Card.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
 
-function Card() {
+function Card({data}) {
   return (
     <div>
-      Lorem 
+      <div className="max-w-[320px] mt-2 bg-zinc-800 flex flex-col p-3  rounded-sm shadow-lg">
+        <img
+          className="w-full h-[240px] rounded-sm"
+          src={data.image}
+          alt=""
+        />
+        <h1 className="mt-4 text-rose-500 text-[1.1rem] font-semibold">$ {data.price}</h1>
+        <h1 className="mt-[4px] text-2xl font-bold text-white/90">{data.place}</h1>
+        <p className="mt-[4px] text-sm text-white/30 tracking-normal">{data.desc} 
+          <span>
+            <a href="#" className="text-blue-400">
+              &nbsp; read more
+            </a>
+          </span>{" "}
+        </p>
+
+        <button className="boder-none bg-rose-500 hover:bg-rose-900 text-white/90 mt-5 p-1 font-medium text-[15px] rounded-sm">
+          Not Interested
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
