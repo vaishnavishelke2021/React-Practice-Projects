c98596ed118dbfa30fc45a0f44613d0ad1108f27
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-20 13:23:56 +0530

    added form

---
+++ Form_State_Management/form-state/src/Components/Form.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+      <div className='main-container'>
+        <div className='box-1'></div>
+        <form className='box-2' action="">
+asfac
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Form
