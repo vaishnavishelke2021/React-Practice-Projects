d0ac808850f57b24dd218d8fe5c8de9fc4fe93c0
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-22 17:20:56 +0530

    added search gif

--- Random_GIF/gif-generator/src/Components/Custom.js
+++ Random_GIF/gif-generator/src/Components/Custom.js
@@ -1,16 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const API_KEY = "mnV8KJUIQJYrHN3HI2LQx1wUcrMrF2ZP";
 
 function Custom() {
+  const [gifUrl, setGifUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [tag, setTag] = useState("cat");
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+      const response = await axios.get(url);
+      setGifUrl(response.data.data.images.downsized_large.url);
+    } catch (error) {
+      console.error("Error fetching GIF:", error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const clickHandler = () => {
+    fetchData();
+  };
+
+  const changeHandler = (e) => {
+    setTag(e.target.value);
+  };
+
   return (
-    <div className='w-[35%] bg-[#95d2b41b] shadow-md p-4 gap-y-2 flex flex-col justify-center items-center rounded-md'>
-       <h1 className='text-[#f6f6f6] text-2xl font-semibold '>Custom GIF</h1>
-       <div className='w-full h-[17rem] mt-3 flex justify-center items-center'>
-        <img className='h-full' src="https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1" alt="" />
-       </div>
-       <input className='w-full py-1 px-3 mt-4 bg-white rounded-sm' placeholder='Enter here...' type="text" />
-       <button className='bg-[#C39898] w-full py-1 text-white text-[1.2rem] font-semibold'>Generate</button>
+    <div className="w-[35%] bg-[#c0bebe23] shadow-md p-4  flex flex-col items-center rounded-md">
+      <h1 className="text-[#f6f6f6] text-2xl font-semibold ">
+        Random {tag} GIF
+      </h1>
+      <div className="w-full h-[15rem] bg-white/10 rounded-sm mt-3 flex justify-center items-center">
+        {!loading ? (
+          <img className="h-full " src={gifUrl} alt="Random GIF" />
+        ) : (
+          <p className="text-white spinner"></p>
+        )}
+      </div>
+      <input
+        className="w-full text-center font-bold py-1 px-3 mt-4 bg-white rounded-sm"
+        placeholder="Enter here..."
+        type="text"
+        value={tag}
+        onChange={changeHandler}
+      />
+      <button
+        onClick={clickHandler}
+        className="bg-[#436724] mt-4 w-full py-1 text-white text-[1.2rem] font-semibold"
+      >
+        Generate New GIF
+      </button>
     </div>
-  )
+  );
 }
 
-export default Custom
+export default Custom;
--- Random_GIF/gif-generator/src/Components/Random.js
+++ Random_GIF/gif-generator/src/Components/Random.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+const API_KEY = "mnV8KJUIQJYrHN3HI2LQx1wUcrMrF2ZP";
 
 function RandomGif() {
-  // Descriptive name
   const [gifUrl, setGifUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const fetchData = async () => {
+    setLoading(true);
     try {
       const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
       const response = await axios.get(url);
@@ -15,24 +16,29 @@ function RandomGif() {
     } catch (error) {
       console.error("Error fetching GIF:", error);
     }
+    setLoading(false);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const clickHandler = () => {
+    fetchData();
+  };
+
   return (
-    <div className="w-[35%] bg-[#c0bebe23] shadow-md p-4 gap-y-2 flex flex-col justify-center items-center rounded-md">
+    <div className="w-[35%] bg-[#c0bebe23] shadow-md p-4 flex flex-col  items-center rounded-md">
       <h1 className="text-[#f6f6f6] text-2xl font-semibold ">Random GIF</h1>
-      <div className="w-full h-[19rem] bg-white/10 rounded-sm mt-3 flex justify-center items-center">
-        {gifUrl ? ( // Conditionally render the image
+      <div className="w-full h-[17rem] bg-white/10 rounded-sm mt-3 flex justify-center items-center">
+        {!loading ? (
           <img className="h-full " src={gifUrl} alt="Random GIF" />
         ) : (
-          <p className="text-white">Loading...</p>
+          <p className="text-white spinner"></p>
         )}
       </div>
       <button
-        onClick={fetchData}
+        onClick={clickHandler}
         className="bg-[#436724] mt-4 w-full py-1 text-white text-[1.2rem] font-semibold"
       >
         Generate New GIF
