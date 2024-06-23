8d2d05d9da34b976fa3d62ba09c70524694fa31b
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-23 23:01:39 +0530

    responsiveness added

--- Tech_Blogs/tech-blogs/src/Components/Header.js
+++ Tech_Blogs/tech-blogs/src/Components/Header.js
@@ -5,7 +5,7 @@ import { AppContext } from "../AppContext";
 function Header() {
   const { isDark } = useContext(AppContext);
   return (
-    <div className={`w-[100%] mx-auto py-4 px-[18rem] flex justify-between items-center fixed top-0 shadow-md ${isDark ? 'bg-dark' : 'bg-light'}`}>
+    <div className={`w-[100%] mx-auto py-4 px-[2rem] md:px-[10rem] flex justify-between items-center fixed top-0 shadow-md ${isDark ? 'bg-dark' : 'bg-light'}`}>
       <h1 className="lg:text-[1.7rem] text-2xl font-bold">
         <span className={`${isDark ? 'text-darkBlue' : 'text-primary'}`}>Tech</span>Blogs
       </h1>
--- Tech_Blogs/tech-blogs/src/Components/Pagination.js
+++ Tech_Blogs/tech-blogs/src/Components/Pagination.js
@@ -7,11 +7,13 @@ function Pagination() {
   const { page, totalPages, handlePageChange, isDark } = useContext(AppContext);
 
   return (
-    <div className="w-[100%] mx-auto py-[9px] px-[18rem] flex justify-between items-center fixed bottom-0 bg-primary">
+    <div className="w-[100%] mx-auto py-2 px-[2rem] md:px-[10rem] flex justify-between items-center fixed bottom-0 bg-primary">
       <div className="space-x-4">
         {page > 1 && (
           <button
-            className={`${isDark ? 'bg-dark text-light' : 'bg-light text-dark'} py-1 px-[3px] rounded-sm mt-1`}
+            className={`${
+              isDark ? "bg-dark text-light" : "bg-light text-dark"
+            } py-1 px-[2.5px] rounded-sm mt-1`}
             onClick={() => handlePageChange(page - 1)}
           >
             <IoIosArrowBack />
@@ -20,7 +22,9 @@ function Pagination() {
 
         {page < totalPages && (
           <button
-            className={`${isDark ? 'bg-dark text-light' : 'bg-light text-dark'} py-1 px-[3px] rounded-sm`}
+            className={`${
+              isDark ? "bg-dark text-light" : "bg-light text-dark"
+            } py-1 px-[2.5px] rounded-sm`}
             onClick={() => handlePageChange(page + 1)}
           >
             <IoIosArrowForward />
