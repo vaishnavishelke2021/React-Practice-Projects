cf6467069c8772fe3ce559af646f8f9241e5ba11
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-23 22:39:36 +0530

    light-dark theme added

--- Tech_Blogs/tech-blogs/src/Components/Theme.js
+++ Tech_Blogs/tech-blogs/src/Components/Theme.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { MdLightMode } from "react-icons/md";
+import { AppContext } from "../AppContext";
+import { MdDarkMode } from "react-icons/md";
 
 function Theme() {
+  const { isDark, setIsDark } = useContext(AppContext);
+
   return (
-    <div className="bg-primary rounded-full text-[1rem] p-[6px]">
-      <MdLightMode />
+    <div className="">
+      <button className="bg-primary hover:scale-110 rounded-full text-[1rem] p-[5px] transition-all cursor-pointer" 
+      onClick={(prev) => setIsDark(!isDark)}>
+        {isDark ? (
+          <MdDarkMode className="text-light" />
+        ) : (
+          <MdLightMode className="text-light" />
+        )}
+      </button>
     </div>
   );
 }
