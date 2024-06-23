38f1ded674440fe9ed07b148cd265900994d0a3f
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-23 18:08:56 +0530

    header added

--- Tech_Blogs/tech-blogs/src/Components/Header.js
+++ Tech_Blogs/tech-blogs/src/Components/Header.js
@@ -1,13 +1,15 @@
-import React from 'react'
-// import Theme from './Theme'
+import React from "react";
+import Theme from "./Theme";
 
 function Header() {
   return (
-    <div className='w-[90%] mx-auto py-5 flex justify-between items-center'>
-        
-      
+    <div className="w-[90%] mx-auto py-5 flex justify-between items-center">
+      <h1 className="lg:text-3xl text-2xl font-bold">
+        <span className="text-darkBlue">Tech</span>Blogs
+      </h1>
+      <Theme />
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
