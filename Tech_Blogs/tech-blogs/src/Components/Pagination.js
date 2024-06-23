8c9703bc46407be9066c035f38e74cfa9c202191
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-23 21:48:19 +0530

    pagination added

--- Tech_Blogs/tech-blogs/src/Components/Pagination.js
+++ Tech_Blogs/tech-blogs/src/Components/Pagination.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "../AppContext";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 function Pagination() {
+  const { page, totalPages, handlePageChange } = useContext(AppContext);
+
   return (
-    <div className='w-[90%] mx-auto py-3'>
-      p
+    <div className="w-[90%] mx-auto py-5 flex justify-between items-center">
+      <div className="space-x-4">
+        {page > 1 && (
+          <button
+            className="bg-lightBlue text-dark py-1 px-[3px] rounded-sm"
+            onClick={() => handlePageChange(page - 1)}
+          >
+            <IoIosArrowBack />
+          </button>
+        )}
+
+        {page < totalPages && (
+          <button
+            className="bg-lightBlue text-dark py-1 px-[3px] rounded-sm"
+            onClick={() => handlePageChange(page + 1)}
+          >
+            <IoIosArrowForward />
+          </button>
+        )}
+      </div>
+      <p className="text-[15px] font-normal">
+        Page <span className="font-bold">{page}</span> of {totalPages}
+      </p>
     </div>
-  )
+  );
 }
 
-export default Pagination
+export default Pagination;
