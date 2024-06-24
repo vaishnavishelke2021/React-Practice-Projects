c900d93378148649a7d3fa724c072fcebd3a0fe9
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-24 13:24:49 +0530

    css updated

--- Tech_Blogs/tech-blogs/src/Components/Card.js
+++ Tech_Blogs/tech-blogs/src/Components/Card.js
@@ -10,8 +10,8 @@ function Card({ post }) {
         isDark ? "bg-light/5" : "bg-dark/5"
       } rounded-sm`}
     >
-      <h1 className="text-[1.4rem] font-bold">{post.title}</h1>
-      <p className="text-[15px] font-normal tracking-[0.7px] mt-1">
+      <h1 className="text-[1.4rem] font-bold leading-[26px]">{post.title}</h1>
+      <p className="text-[15px] font-normal tracking-[0.7px] mt-2">
         By <span className="italic">{post.author}</span>on{" "}
         <span className="underline font-semibold">{post.category}</span>
       </p>
@@ -21,7 +21,7 @@ function Card({ post }) {
       <h2 className="mt-6 leading-[24px] font-medium text-[17px]">
         {post.content}
       </h2>
-      <div className="flex space-x-3 flex-wrap underline text-blue-500 font-medium text-sm mt-2">
+      <div className="flex gap-x-2 flex-wrap underline text-blue-500 font-medium text-sm mt-2">
         {post.tags.map((tag, i) => (
           <p>#{tag}</p>
         ))}
