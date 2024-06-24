326096e845aabfd3843c6879c0518f6bacdfb997
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-24 20:07:38 +0530

    CategoryPage & TagPage added

--- Tech_Blogs/tech-blogs/src/Components/CategoryPage.js
+++ Tech_Blogs/tech-blogs/src/Components/CategoryPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useContext } from "react";
+import Header from "./Header";
+import { useNavigate, useLocation } from "react-router-dom";
+import Blogs from "./Blogs";
+import Pagination from "./Pagination";
+import { AppContext } from "../AppContext";
 
 function CategoryPage() {
+  const { isDark } = useContext(AppContext);
+  const navigation = useNavigate();
+  const location = useLocation();
+  const category = location.pathname.split("/").at(-1);
   return (
-    <div>
-      Category
+    <div
+      className={`font-Catamaran h-full ${
+        isDark ? " bg-dark text-light" : " bg-light text-dark"
+      }`}
+    >
+      <Header />
+      <div>
+        {<button onClick={() => navigation(-1)}>Back</button>}
+        <h1>
+          Blogs on <span>#{category}</span>
+        </h1>
+      </div>
+      <Blogs />
+      <Pagination />
     </div>
-  )
+  );
 }
 
-export default CategoryPage
+export default CategoryPage;
--- Tech_Blogs/tech-blogs/src/Components/TagPage.js
+++ Tech_Blogs/tech-blogs/src/Components/TagPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useContext } from "react";
+import Header from "./Header";
+import { useNavigate, useLocation } from "react-router-dom";
+import Blogs from "./Blogs";
+import Pagination from "./Pagination";
+import { AppContext } from "../AppContext";
 
 function TagPage() {
+  const { isDark } = useContext(AppContext);
+  const navigation = useNavigate();
+  const location = useLocation();
+  const tag = location.pathname.split("/").at(-1);
   return (
-    <div>
-      Tag
+    <div
+      className={`font-Catamaran h-full ${
+        isDark ? " bg-dark text-light" : " bg-light text-dark"
+      }`}
+    >
+      <Header />
+      <div className="h-[]">
+        {<button onClick={() => navigation(-1)}>Back</button>}
+        <h1>
+          Blogs Tagged <span>#{tag}</span>
+        </h1>
+      </div>
+      <Blogs />
+      <Pagination />
     </div>
-  )
+  );
 }
 
-export default TagPage
+export default TagPage;
