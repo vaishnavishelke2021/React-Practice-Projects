2ab113a9caaa480b786e7c08afd3b6e50d17a85f
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-24 21:10:07 +0530

    page updated

--- Tech_Blogs/tech-blogs/src/Components/BlogPage.js
+++ Tech_Blogs/tech-blogs/src/Components/BlogPage.js
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { AppContext } from "../AppContext";
-import { API_URL } from "../constants";
 import Header from "./Header";
 import Card from "./Card";
 
 function BlogPage() {
   const { loading, setLoading, isDark } = useContext(AppContext);
+  const newURL = "https://codehelp-apis.vercel.app/api/";
 
   const [blog, setBlog] = useState(null);
   const [relatedBlogs, setRelatedBlogs] = useState([]);
@@ -17,7 +17,7 @@ function BlogPage() {
 
   const fetchRelatedBlogs = async () => {
     setLoading(true);
-    let url = `${API_URL}?blogId=${blogId}`;
+    let url = `${newURL}get-blog?blogId=${blogId}`;
     try {
       const response = await fetch(url);
       const data = await response.json();
@@ -38,28 +38,45 @@ function BlogPage() {
   }, [location.pathname]);
 
   return (
-    <div className={`font-Catamaran h-full ${
+    <div
+      className={`font-Catamaran h-full ${
         isDark ? " bg-dark text-light" : " bg-light text-dark"
-      }`}>
+      }`}
+    >
       <Header />
-      <div className="w-full h-[100vh] md:w-[63%] pt-24 mx-auto py-3 pb-12">
-        <button onClick={() => navigation(-1)}>Back</button>
-
+      <div className="w-full h-full md:w-[63%] pt-24 mx-auto py-3 pb-12">
         <div>
           {loading ? (
-            <p>Loading</p>
+            <div className="flex justify-center items-center w-full h-screen">
+              <div className="spinner"></div>
+            </div>
           ) : blog ? (
             <div>
-              <Card blog={blog} />
+              <div className="max-w-[80%] mx-auto">
+                <button
+                  className="bg-primary text-light
+            p-1 px-4 rounded-sm mt-1 -mb-3"
+                  onClick={() => navigation(-1)}
+                >
+                  Back
+                </button>
+              </div>
+
+              <Card post={blog} />
+
+              <div className="max-w-[80%] mx-auto text-3xl font-bold mt-14 -mb-3">
+                <h1>Related Blogs</h1>
+              </div>
+              {relatedBlogs.map(
+                (post) => post && <Card key={post.id} post={post} />
+              )}
             </div>
           ) : (
-            <p>No Blog Found</p>
+            <div className="flex justify-center items-center w-full h-screen">
+              <p className="font-bold text-xl">No Blog Found</p>
+            </div>
           )}
         </div>
-        <h1>Related Blogs</h1>
-        {/* {relatedBlogs.map((post) => (
-          <Card key={post.id} post={post} />
-        ))} */}
       </div>
     </div>
   );
