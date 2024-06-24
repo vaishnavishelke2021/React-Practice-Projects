7921c5ef3d4b95d81d0da974ac51038c733d4e8e
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-24 19:43:34 +0530

    updated useEffect

--- Tech_Blogs/tech-blogs/src/App.js
+++ Tech_Blogs/tech-blogs/src/App.js
@@ -7,8 +7,7 @@ import TagPage from "./Components/TagPage";
 import CategoryPage from "./Components/CategoryPage";
 
 export default function App() {
-  const { fetchData, category } = useContext(AppContext);
-  
+  const { fetchData } = useContext(AppContext);
 
   const [searchParams, setSearchParams] = useSearchParams();
   const location = useLocation();
@@ -17,18 +16,20 @@ export default function App() {
     const page = searchParams.get("page") ?? 1;
 
     if (location.pathname.includes("tags")) {
-      const tag = location.pathname.split("/").at(-1).replaceAll("-", " ");
+      const tag = location.pathname.split("/").at(-1)?.replaceAll("-", " ");
       fetchData(Number(page), tag);
     } else if (location.pathname.includes("categories")) {
-      const tag = location.pathname.split("/").at(-1).replaceAll("-", " ");
+      const category = location.pathname
+        .split("/")
+        .at(-1)
+        ?.replaceAll("-", " ");
       fetchData(Number(page), null, category);
     } else {
       fetchData(Number(page));
     }
-  }, [location.pathname, location.search]);
+  }, [location.pathname]);
 
   return (
-    
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/blog/:blogId" element={<BlogPage />} />
