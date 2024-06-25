8716f9a7295de99bea210570cda69e6dbc779cd0
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-25 17:40:11 +0530

    react-router-dom installed and routes added

--- Shopping_Cart/shopping-cart/src/App.js
+++ Shopping_Cart/shopping-cart/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import { Routes, Route } from "react-router-dom";
+import Cart from "./Components/Cart";
 
 function App() {
   return (
     <div>
       <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
