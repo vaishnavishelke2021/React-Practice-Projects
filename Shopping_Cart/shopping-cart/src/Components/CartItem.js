7876c079f6888daff151c562330ec8a329c27ffb
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-26 00:17:18 +0530

    remove from cart functionality added

--- Shopping_Cart/shopping-cart/src/Components/CartItem.js
+++ Shopping_Cart/shopping-cart/src/Components/CartItem.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { MdDelete } from "react-icons/md";
+import { removeItem } from "../redux/cartSlice";
+import { useDispatch } from "react-redux";
 
 function CartItem({ item }) {
+  const dispatch = useDispatch();
 
-    const desc = item.description.substring(0, 160);
+  const removeFromItem = () => {
+    dispatch(removeItem(item.id));
+  };
+
+  const desc = item.description.substring(0, 160);
   return (
     <div className="max-w-full bg-white flex justify-between p-3 rounded-md">
       <div className="max-w-[30%] flex justify-center item-center">
@@ -15,8 +22,13 @@ function CartItem({ item }) {
         <p className="text-[12px] mt-1 text-black/50">{desc}...</p>
 
         <div className="flex justify-between mt-3">
-          <h1 className="text-lg font-semibold text-green-700">$ {item.price}</h1>
-          <MdDelete className="p-[7px] text-[1.9rem] text-rose bg-rose/20 rounded-full cursor-pointer transition-all duration-[0.3s] hover:bg-rose hover:text-[#f6f6f6]" />
+          <h1 className="text-lg font-semibold text-green-700">
+            $ {item.price}
+          </h1>
+          <MdDelete
+            onClick={removeFromItem}
+            className="p-[7px] text-[1.9rem] text-rose bg-rose/20 rounded-full cursor-pointer transition-all duration-[0.3s] hover:bg-rose hover:text-[#f6f6f6]"
+          />
         </div>
       </div>
     </div>
