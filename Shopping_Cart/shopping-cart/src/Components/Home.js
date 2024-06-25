638dafba746a911caf8014215d70920de5a2efaf
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-25 18:25:28 +0530

    fetched API

--- Shopping_Cart/shopping-cart/src/Components/Home.js
+++ Shopping_Cart/shopping-cart/src/Components/Home.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Home() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const API_URL = "https://fakestoreapi.com/products";
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const fetchProductData = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(API_URL);
+      const data = await res.json();
+      setProducts(data);
+    } catch (e) {
+      console.log(e);
+      console.log("Error fetching data");
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, []);
+
+  return <div></div>;
 }
 
-export default Home
+export default Home;
