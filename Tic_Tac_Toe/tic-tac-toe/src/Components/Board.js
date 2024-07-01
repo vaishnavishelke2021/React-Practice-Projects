fb60a19a0ca2042aa46b9e894e3857a188c5a358
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-01 20:03:05 +0530

    checkWinner logic updated

--- Tic_Tac_Toe/tic-tac-toe/src/Components/Board.js
+++ Tic_Tac_Toe/tic-tac-toe/src/Components/Board.js
@@ -22,37 +22,47 @@ function Board() {
       [1, 4, 7],
       [2, 5, 8],
       [2, 4, 6],
-      [1, 4, 8],
+      [0, 4, 8],
     ];
 
     for (let w of winner) {
       const [a, b, c] = w;
       if (state[a] !== null && state[a] === state[b] && state[a] === state[c]) {
-        return true;
+        return state[a];
       }
-      return false;
     }
+    return false;
   };
 
   const isWinner = checkWinner();
 
   return (
     <div className="board">
-      <div className="row">
-        <Square onClick={() => handleClick(0)} value={state[0]} />
-        <Square onClick={() => handleClick(1)} value={state[1]} />
-        <Square onClick={() => handleClick(2)} value={state[2]} />
-      </div>
-      <div className="row">
-        <Square onClick={() => handleClick(3)} value={state[3]} />
-        <Square onClick={() => handleClick(4)} value={state[4]} />
-        <Square onClick={() => handleClick(5)} value={state[5]} />
-      </div>
-      <div className="row">
-        <Square onClick={() => handleClick(6)} value={state[6]} />
-        <Square onClick={() => handleClick(7)} value={state[7]} />
-        <Square onClick={() => handleClick(8)} value={state[8]} />
-      </div>
+      {isWinner ? (
+        <h2>
+          {" "}
+          &nbsp;<span style={{ color: "green" }}>{isWinner} Won </span> The
+          Game!&nbsp;
+        </h2>
+      ) : (
+        <>
+          <div className="row">
+            <Square onClick={() => handleClick(0)} value={state[0]} />
+            <Square onClick={() => handleClick(1)} value={state[1]} />
+            <Square onClick={() => handleClick(2)} value={state[2]} />
+          </div>
+          <div className="row">
+            <Square onClick={() => handleClick(3)} value={state[3]} />
+            <Square onClick={() => handleClick(4)} value={state[4]} />
+            <Square onClick={() => handleClick(5)} value={state[5]} />
+          </div>
+          <div className="row">
+            <Square onClick={() => handleClick(6)} value={state[6]} />
+            <Square onClick={() => handleClick(7)} value={state[7]} />
+            <Square onClick={() => handleClick(8)} value={state[8]} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
