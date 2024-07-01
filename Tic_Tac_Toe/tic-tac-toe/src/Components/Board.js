ce352cd87f9d82042d8d078a5469f281e29a698f
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-01 19:33:38 +0530

    onClick prop & handleClick function added

--- Tic_Tac_Toe/tic-tac-toe/src/Components/Board.js
+++ Tic_Tac_Toe/tic-tac-toe/src/Components/Board.js
@@ -2,23 +2,27 @@ import React, { useState } from "react";
 import Square from "./Square";
 
 function Board() {
-  const [state, setState] = useState(Array(9).fill('0'));
+  const [state, setState] = useState(Array(9).fill(null));
+
+  const handleClick = (index) => {
+    console.log("clicked", index);
+  };
   return (
     <div className="board">
       <div className="row">
-        <Square value={state[0]} />
-        <Square value={state[1]} />
-        <Square value={state[2]} />
+        <Square onClick={() => handleClick(0)} value={state[0]} />
+        <Square onClick={() => handleClick(1)} value={state[1]} />
+        <Square onClick={() => handleClick(2)} value={state[2]} />
       </div>
       <div className="row">
-        <Square value={state[3]} />
-        <Square value={state[4]} />
-        <Square value={state[5]} />
+        <Square onClick={() => handleClick(3)} value={state[3]} />
+        <Square onClick={() => handleClick(4)} value={state[4]} />
+        <Square onClick={() => handleClick(5)} value={state[5]} />
       </div>
       <div className="row">
-        <Square value={state[6]} />
-        <Square value={state[7]} />
-        <Square value={state[8]} />
+        <Square onClick={() => handleClick(6)} value={state[6]} />
+        <Square onClick={() => handleClick(7)} value={state[7]} />
+        <Square onClick={() => handleClick(8)} value={state[8]} />
       </div>
     </div>
   );
--- Tic_Tac_Toe/tic-tac-toe/src/Components/Square.js
+++ Tic_Tac_Toe/tic-tac-toe/src/Components/Square.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Square({ value }) {
+function Square({ value, onClick }) {
   return (
-    <div className="box">
+    <div className="box" onClick={onClick}>
       <h3>{value}</h3>
     </div>
   );
