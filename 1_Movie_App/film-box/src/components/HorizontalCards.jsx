1b3ee2c605196533c63511a20097d16fcdfb4d86
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-07 21:56:19 +0530

    horizontal card section added

---
+++ 1_Movie_App/film-box/src/components/HorizontalCards.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+const HorizontalCards = () => {
+  return (
+    <div className="w-full p-10">
+      <h1 className="text-xl font-semibold mb-6 mt-5">Trending</h1>
+      <Card />
+    </div>
+  );
+};
+
+export default HorizontalCards;
--- 1_Movie_App/film-box/src/components/MainPage.jsx
+++ 1_Movie_App/film-box/src/components/MainPage.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
+import HorizontalCards from "./HorizontalCards";
 import SearchBar from "./SearchBar";
 
 const MainPage = () => {
   return (
-    <div className="w-[82%] p-2">
+    <div className="w-[82%] p-2 overflow-hidden overflow-y-auto">
       <SearchBar />
       <Header />
+      <HorizontalCards />
     </div>
   );
 };
