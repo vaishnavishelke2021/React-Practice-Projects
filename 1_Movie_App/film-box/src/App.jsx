33886011f7be4679e01cc391dde6b64f78c1dde8
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-09 22:26:02 +0530

    /trending route created

--- 1_Movie_App/film-box/src/App.jsx
+++ 1_Movie_App/film-box/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Trending from "./components/Trending";
 
 const App = () => {
   return (
     <div className="w-full h-screen">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/trending" element={<Trending />} />
       </Routes>
     </div>
   );
