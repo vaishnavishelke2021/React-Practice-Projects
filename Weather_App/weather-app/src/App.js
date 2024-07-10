0bfc6b385dbed895430922ea78534a8a6af1cde2
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-10 20:18:36 +0530

    initial updates

--- Weather_App/weather-app/src/App.js
+++ Weather_App/weather-app/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>
-      Weather
+    <div className="bg-zinc-900 w-full h-screen text-zinc-100 flex justify-center items-center">
+      <div className="w-90% mx-auto">
+        <h1 className="text-3xl font-semibold">Weather App</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
