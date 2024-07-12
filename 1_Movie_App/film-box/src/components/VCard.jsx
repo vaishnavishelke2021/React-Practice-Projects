5375c7b44d4d9788329115005ec1d16ddb55c20b
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-12 15:22:37 +0530

    Card updated

--- 1_Movie_App/film-box/src/components/VCard.jsx
+++ 1_Movie_App/film-box/src/components/VCard.jsx
@@ -1,20 +1,24 @@
 const VCard = ({ t }) => {
   return (
-    <div className="w-[18.3%] bg-[#1e1e1e] p-2 rounded-[4px] mb-2 hover:scale-[1.1] transition-all duration-200 ease-in-out cursor-pointer hover:shadow-xl">
+    <div className="relative w-[23.5%] bg-[#1e1e1e] p-2 rounded-[4px] mb-2 hover:scale-[1.07] transition-all duration-200 ease-in-out cursor-pointer hover:shadow-xl">
       <img
-        className="w-full h-[240px] object-cover rounded-[2px]"
+        className="w-full h-[200px] object-cover rounded-[2px]"
         src={`https://image.tmdb.org/t/p/original/${
           t.backdrop_path || t.poster_path
         }`}
         alt=""
       />
 
-      <h1 className="text-[1.2rem] font-extrabold mt-2">
+      <h1 className="text-[1.2rem] font-bold mt-2">
         {t.name || t.title || t.original_name || t.original_title}
       </h1>
-      <p className="text-[14px] text-secondary/60 mt-2">
-        {t.overview.slice(0, 40)} <span className="text-zinc-600">more...</span>
+      <p className="text-[14px] text-secondary/40 mt-2">
+        {t.overview.slice(0, 30)} <span className="text-zinc-600">more...</span>
       </p>
+
+      <div className="absolute top-3 right-3 shadow-lg bg-orange text-[13.5px] text-secondary text-extrabold p-2 flex justify-center items-center rounded-full w-[35px] h-[35px]">
+        {(t.vote_average * 10).toFixed()} <span className="text-[10px]">%</span>
+      </div>
     </div>
   );
 };
