070f1818af8c0a982bc5603c0102e8ce947451a8
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-13 17:00:54 +0530

    movieActions created

--- 1_Movie_App/film-box/src/components/MovieDetails.jsx
+++ 1_Movie_App/film-box/src/components/MovieDetails.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { asyncloadmovie, removeMovie } from "../store/actions/movieActions";
+import {  useParams } from "react-router-dom";
 
 const MovieDetails = () => {
-  return <div className="">MovieDetails</div>;
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(asyncloadmovie(id));
+    return () => {
+      dispatch(removeMovie());
+    };
+  });
+  return (
+    <div className="w-screen h-screen p-10">
+      
+    </div>
+  );
 };
 
 export default MovieDetails;
--- 1_Movie_App/film-box/src/store/actions/movieActions.jsx
+++ 1_Movie_App/film-box/src/store/actions/movieActions.jsx
@@ -1,5 +1,30 @@
 export { removeMovie } from "../reducers/movieSlice";
 
 import axios from "../../utils/axios";
-
 import { loadMovie } from "../reducers/movieSlice";
+
+// import { loadMovie } from "../reducers/movieSlice";
+
+export const asyncloadmovie = (id) => async (dispatch, getState) => {
+  try {
+    const detail = await axios.get(`/movie/${id}`);
+    const externalid = await axios.get(`/movie/${id}/external_ids`);
+    const recommendations = await axios.get(`/movie/${id}/recommendations`);
+    const similar = await axios.get(`/movie/${id}/similar`);
+    const videos = await axios.get(`/movie/${id}/videos`);
+    const watchproviders = await axios.get(`/movie/${id}/watch/providers`);
+    let theUltimateData = {
+      detail: detail.data,
+      externalid: externalid.data,
+      recommendations: recommendations.data.results,
+      similar: similar.data.results,
+      videos: videos.data.results.find((m) => m.type === "Trailer"),
+      watchproviders: watchproviders.data.results,
+    };
+
+    dispatch(loadMovie(theUltimateData));
+    console.log(theUltimateData);
+  } catch (e) {
+    console.log(e);
+  }
+};
