7b3f486aef1859002f7064914bec8ccabde0d7c7
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-14 12:04:47 +0530

    movieDetails page header created

--- 1_Movie_App/film-box/src/components/MovieDetails.jsx
+++ 1_Movie_App/film-box/src/components/MovieDetails.jsx
@@ -1,21 +1,205 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { asyncloadmovie, removeMovie } from "../store/actions/movieActions";
-import {  useParams } from "react-router-dom";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import { FaGlobeAmericas } from "react-icons/fa";
+import { FaLink } from "react-icons/fa6";
+import { LiaImdb } from "react-icons/lia";
+import { IoMdArrowBack } from "react-icons/io";
 
 const MovieDetails = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
   const { id } = useParams();
   const dispatch = useDispatch();
+  const { info } = useSelector((state) => state.movie);
+  console.log(info);
   useEffect(() => {
     dispatch(asyncloadmovie(id));
     return () => {
       dispatch(removeMovie());
     };
-  });
-  return (
-    <div className="w-screen h-screen p-10">
-      
+  }, [id]);
+
+  return info ? (
+    <div className="w-full mx-auto  h-screen">
+      <div
+        style={{
+          background: `linear-gradient(rgba(0, 0, 0, 0.77), rgba(0,0,0,.79), rgba(0,0,0,.81)), url(https://image.tmdb.org/t/p/original/${info.detail.backdrop_path})`,
+          backgroundPosition: "top 1%",
+          backgroundSize: "cover",
+        }}
+        className="w-full h-[85vh] px-6 py-4 rounded-sm flex items-center"
+      >
+        {/* -----------------------------poster and details overvoew------------------------------ */}
+        <div className="w-[90%] mx-auto ">
+          {/* ------------------------------back btn------------------------------- */}
+
+          <Link
+            onClick={() => navigate(-1)}
+            className=" text-white cursor-pointer text-2xl  hover:text-orange transition-all duration-200 w-fit"
+          >
+            <IoMdArrowBack />
+          </Link>
+
+          <div className=" flex justify-between mt-[3.2rem]">
+            {/* -----------------------------poster------------------------------ */}
+            <div className="w-[22%] flex flex-col gap-y-0">
+              <img
+                className="w-full h-[55vh] object-cover rounded-[3px] "
+                src={`https://image.tmdb.org/t/p/original/${
+                  info.detail.backdrop_path || info.detail.poster_path
+                }`}
+                alt=""
+              />
+              <div>
+                {info.watchproviders && info.watchproviders.flatrate && (
+                  <div className="flex gap-x-3 bg-white/5 shadow-lg p-4 justify-center items-center">
+                    <div className="flex flex-row gap-x-3">
+                      {info.watchproviders.flatrate.map((w, i) => (
+                        <img
+                          className="w-[5.5vh] h-[5.5vh] rounded-sm object-cover"
+                          key={i}
+                          src={`https://image.tmdb.org/t/p/original/${w.logo_path}`}
+                        />
+                      ))}
+                    </div>
+                    <div className="flex flex-col leading-4">
+                      <h1 className="text-sm text-secondary/80">
+                        Now Streaming
+                      </h1>
+                      <h1 className="text-sm font-semibold">Watch Now</h1>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+
+            {/* --------------------details and overview--------------------------- */}
+            <div className="text-start w-[72%] ">
+              <h1 className="text-4xl font-bold">
+                {`${info.detail.title} `}
+                <span className="font-normal text-2xl text-white/60">
+                  ({new Date(info.detail.release_date).getFullYear()})
+                </span>
+              </h1>
+
+              <div className="mt-1">
+                {/* ------------------------------------release date & genre----------------------------------------- */}
+                <div className="flex gap-x-4 text-[14.4px] mt-1 text-secondary/70">
+                  <h1>{info.detail.release_date}</h1> |
+                  <p className="flex gap-x-2">
+                    {info.detail.genres.map((g) => g.name).join(",  ")}
+                  </p>
+                  |
+                  <p>
+                    {`${Math.floor(info.detail.runtime / 60)}h ${
+                      info.detail.runtime % 60
+                    }m`}
+                  </p>
+                </div>
+
+                {/* -----------------------------------user score , links----------------------------------- */}
+                <div className="flex space-x-8  items-center">
+                  <div className="flex space-x-3 mt-10">
+                    <div className="ashadow-lg bg-green-700 text-[18px] text-secondary text-extrabold p-2 flex justify-center items-center rounded-full w-[45px] h-[45px]">
+                      {(info.detail.vote_average * 10).toFixed()}
+                      <span className="text-[10px]">%</span>
+                    </div>
+
+                    <h1 className="font-medium leading-5 mt-[2px]">
+                      User <br /> Score
+                    </h1>
+                  </div>
+
+                  <nav className="flex gap-x-4 mt-6 items-center">
+                    <a
+                      href={`https://www.wikidata.org/wiki/${info.externalid.wikidata_id}`}
+                      target="_blank"
+                      className="text-[17px] text-secondary/60 shadow-md hover:text-secondary "
+                    >
+                      <FaGlobeAmericas />
+                    </a>
+                    <a
+                      href={info.detail.homepage}
+                      target="_blank"
+                      className="text-[20px] text-secondary/60 shadow-md hover:text-secondary"
+                    >
+                      <FaLink />
+                    </a>
+                    <a
+                      href={`https://www.imdb.com/title/${info.externalid.imdb_id}/`}
+                      target="_blank"
+                      className="text-[26px] text-secondary/60 shadow-md hover:text-secondary"
+                    >
+                      <LiaImdb />
+                    </a>
+                  </nav>
+                </div>
+
+                {/* -----------tagline, overview----------  */}
+                <p className="mt-10 text-secondary/50 italic">
+                  {info.detail.tagline || "No tagline"}
+                </p>
+                <h5 className="mt-1 text-[18px] font-semibold">Overview</h5>
+                <p className="mt-1 text-secondary/80">{info.detail.overview}</p>
+
+                {/* -------------------trailer link --------------- */}
+                <div className="mt-10">
+                  <Link
+                    target="_blank"
+                    to={`${pathname}/trailer`}
+                    className="bg-orange px-4 py-2 w-fit rounded-[4px] hover:bg-secondary hover:text-primary transition-all duration-200"
+                  >
+                    Watch Trailer
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* -------------------------------------watch providers------------------------------------- */}
+          <div className="mt-10 flex gap-x-6">
+            <div>
+              {info.watchproviders && info.watchproviders.rent && (
+                <div className="flex flex-col gap-y-1">
+                  <h1 className="text-sm">Rent/ Buy</h1>
+                  <div className="flex flex-row gap-x-3">
+                    {info.watchproviders.reninfo.detail.map((w, i) => (
+                      <img
+                        className="w-[5vh] h-[5vh] rounded-sm object-cover"
+                        key={i}
+                        src={`https://image.tmdb.org/t/p/original/${w.logo_path}`}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+
+            {/* <div>
+              {info.watchproviders && info.watchproviders.buy && (
+                <div className="flex flex-col gap-y-1">
+                  <h1 className="text-sm">Available to buy</h1>
+                  <div className="flex flex-row gap-x-3">
+                    {info.watchproviders.buy.map((w, i) => (
+                      <img
+                        className="w-[5vh] h-[5vh] rounded-sm object-cover"
+                        key={i}
+                        src={`https://image.tmdb.org/t/p/original/${w.logo_path}`}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div> */}
+            {/* ------------------------------------------------------------------------------  */}
+          </div>
+        </div>
+      </div>
     </div>
+  ) : (
+    <h1>Loading...</h1>
   );
 };
 
