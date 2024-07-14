a897bb9ea2ce590b32bc27a24bdabb4e7ed6a74f
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-14 16:12:24 +0530

    tvDetails page added

--- 1_Movie_App/film-box/src/App.jsx
+++ 1_Movie_App/film-box/src/App.jsx
@@ -24,7 +24,9 @@ const App = () => {
         </Route>
 
         <Route path="/tvshows" element={<TVShows />} />
-        <Route path="/tv/details/:id" element={<TvDetails />} />
+        <Route path="/tv/details/:id" element={<TvDetails />}>
+          <Route path="/tv/details/:id/trailer" element={<Trailer />} />
+        </Route>
         <Route path="/person" element={<People />} />
         <Route path="/person/details/:id" element={<PersonDetails />} />
         <Route path="*" element={<Error />} />
