c99632814dfc3b9edff3d664827dacfbae07ef85
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-22 21:48:40 +0530

    card created

---
+++ Shopping_Page/shopping-page/src/Components/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="w-[23%] group hover:shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] hover:scale-[1.03] hover:bg-slate-50 transition-all duration-300 cursor-pointer p-3 rounded-lg border-2 bg-primary/10">
+      <div className="max-w-full h-[170px] bg-white rounded-md p-4 flex justify-center items-center">
+        <img
+          className="w-full h-full group-hover:scale-[1.05] transition-all duration-300 cursor-pointer object-contain"
+          src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+          alt=""
+        />
+      </div>
+      <h1 className="font-bold text-[1.1rem] mt-2  transition-all duration-300">
+        Lorem ipsum dolor{" "}
+      </h1>
+      <p className="text-[12px] mt-1 leading-4 text-secondary/60 transition-all duration-300">
+        Lorem ipsum dolor sit amet consectetur adipisicing maiores ad...
+      </p>
+      <h3 className="text-primary font-extrabold text-lg mt-1">$ 200</h3>
+    </div>
+  );
+}
+
+export default Card;
