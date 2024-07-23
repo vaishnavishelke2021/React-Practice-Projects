fbe0cce197caf65881ad5addc21e632353f3345d
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-23 10:32:39 +0530

    Details info added

--- Shopping_Page/shopping-page/src/Components/Details.js
+++ Shopping_Page/shopping-page/src/Components/Details.js
@@ -1,34 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+// import { ProductContext } from "../Context/Context";
 
 const Details = () => {
-  return (
+  const [pdata, setPdata] = useState(null);
+  const { id } = useParams();
+
+  const getSingleProduct = async () => {
+    try {
+      const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+      const data = await res.json();
+      setPdata(data);
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getSingleProduct();
+  });
+
+  return pdata ? (
     <div className="w-full h-screen flex justify-center items-center bg-primary/20">
       <div className="w-[60%] mx-auto flex justify-between p-16 bg-white rounded-lg">
         <div className="w-[35%] h-[300px] shadow-sm border-[2px] bg-white p-6 rounded-md flex justify-center items-center">
           <img
             className="w-full h-full object-contain"
-            src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+            src={pdata?.image}
             alt=""
           />
         </div>
         <div className="w-[58%]">
-          <h1 className="text-[1.7rem] font-extrabold">Lorem Ipsum</h1>
+          <h1 className="text-[1.6rem] leading-8 font-extrabold">
+            {pdata?.title}
+          </h1>
 
-          <p className="mt-3 leading-5 text-[1rem] font-medium text-secondary/80">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error nemo
-            natus laudantium nam magnam dolorem quisquam ullam nihil vero eum?
-            Dolorum dignissimos repudiandae velit illum unde deleniti animi
-            facilis. Aspernatur.
+          <p className="mt-3 leading-[18px] text-[0.95rem] font-medium text-secondary/70">
+            {pdata?.description}
           </p>
 
-          <p className="text-[1rem] font-bold mt-3 text-secondary/70">
-            Category: Men <span>⭐ 3.4</span>
+          <p className="text-[1rem] font-semibold mt-3 text-secondary/40">
+            Category: {pdata?.category} <span className="text-secondary/70">⭐ {pdata?.rating?.rate}</span>
           </p>
 
           <div className="mt-9 flex justify-between items-center">
             <h1 className="text-green-700 font-extrabold text-[1.5rem]">
-              $ 200
+              $ {pdata?.price}
             </h1>
             <div className="flex items-center gap-x-3">
               <Link>
@@ -45,6 +63,10 @@ const Details = () => {
           </div>
         </div>
       </div>
+    </div>
+  ) : (
+    <div className="w-full h-full text-3xl font-bold text-white flex justify-center items-center bg-primary/20">
+      Loading...
     </div>
   );
 };
