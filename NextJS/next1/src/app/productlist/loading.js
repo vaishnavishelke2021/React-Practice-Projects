a767b0cab4a14a54d4b90802c9acd4c94a957a3e
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-11-24 09:42:27 +0530

    loader added

---
+++ NextJS/next1/src/app/productlist/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen w-full bg-amber-200 text-black">
+      Loading...
+    </div>
+  );
+};
+
+export default loading;
