fe1aea4b24d139d87eab0cbd22c5a6d985bc87da
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-05 21:08:53 +0530

    login signup page created

---
+++ NextJSAuth/next-auth/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+export default function LoginPage() {
+  return (
+    <div className="">
+      <h2 className="text-2xl font-extrabold text-center text-black my-10">
+        Login Page
+      </h2>
+
+      <div></div>
+    </div>
+  );
+}
---
+++ NextJSAuth/next-auth/src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+export default function SignUpPage() {
+    return (
+      <div className="">
+        <h2 className="text-2xl font-extrabold text-center text-black my-10">
+          SignUp Page
+        </h2>
+  
+        <div></div>
+      </div>
+    );
+  }
+  
