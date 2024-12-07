2173307f87b0aa6f556802372b312fa273a955b3
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-07 16:10:50 +0530

    userModel created

---
+++ NextJSAuth/next-auth/src/models/userModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+const userschema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+  },
+
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+const User = mongoose.model.users || mongoose.model("users", userschema);
+// const User = mongoose.model('users', userschema);
+
+export default User;
